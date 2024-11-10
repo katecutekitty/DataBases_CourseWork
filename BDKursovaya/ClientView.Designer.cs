1cdb976b82a8eb92f35cd5d31af084a2821fe041
Kate <poznahirko.cat@yandex.ru>
2024-11-10 16:15:15 +0700

    Добавьте файлы проекта.

---
+++ BDKursovaya/ClientView.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace BDKursovaya
+{
+    partial class ClientView
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(204, 360);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(495, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(433, 364);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 278);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(274, 51);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Записаться";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 67);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(274, 28);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(12, 137);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(273, 28);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(11, 214);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(273, 26);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Выбор услуги";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Выбор мастера";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Дата визита";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(308, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(149, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Стоимость услуги:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(306, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(150, 26);
+            this.textBox1.TabIndex = 10;
+            // 
+            // ClientView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 487);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "ClientView";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ BDKursovaya/ClientView.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BDKursovaya
+{
+    public partial class ClientView : Form
+    {
+        public static string s = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=BeautySalon2.mdb;";
+        private OleDbConnection mC;
+        private int currentId = 1;
+
+        public ClientView()
+        {
+            InitializeComponent();
+            mC = new OleDbConnection(s);
+            mC.Open();
+
+            OleDbCommand comm = new OleDbCommand("UPDATE Bookings SET totalPrice = IIf([masterСat] = 'I', [price],IIf([masterСat]= 'II', [price]*2,IIf([masterСat]= 'III', [price]*3,[price]*4)))", mC);
+            comm.ExecuteNonQuery();
+
+            OleDbCommand cmd = new OleDbCommand("SELECT * FROM Services", mC);
+            var l = cmd.ExecuteReader();
+            comboBox1.Items.Clear();
+            while (l.Read()) comboBox1.Items.Add(l[0].ToString());
+            l.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OleDbCommand cmd = new OleDbCommand($"SELECT Bookings.*, Masters.fullName FROM Bookings LEFT JOIN Masters ON Bookings.master_id = Masters.id WHERE client_id = {currentId} AND appointment_date_time > '{DateTime.Now.ToString()}'", mC);
+            var l = cmd.ExecuteReader();
+            listBox1.Items.Clear();
+            while (l.Read())
+            {
+                listBox1.Items.Add(l[3].ToString() + ' ' + l[9].ToString() + ' ' + l[8].ToString() + ' ' + l[5].ToString());
+            }
+            l.Close();
+
+        }
+
+        private int getPrice(string serviceTitle)
+        {
+            int price = Convert.ToInt32(new OleDbCommand($"SELECT price from Services WHERE title = '{serviceTitle}'", mC).ExecuteScalar());
+            return price;
+        }
+
+        private Tuple<int, string> getIdAndCat(string master)
+        {
+            OleDbCommand c = new OleDbCommand($"SELECT id, category FROM Masters WHERE fullName = '{master}'", mC);
+            var l = c.ExecuteReader();
+            Tuple<int, string> result = Tuple.Create(0, "");
+            while (l.Read())
+            {
+                result = Tuple.Create(Convert.ToInt32(l[0]), l[1].ToString());
+            }
+            l.Close();
+            return result;
+        }
+
+        private void RefreshPrice()
+        {
+            int currentPrice = 0;
+            if (comboBox1.SelectedItem != null) {
+                currentPrice = getPrice(comboBox1.SelectedItem.ToString());
+                if (comboBox2.SelectedItem != null)
+                {
+                    string category = getIdAndCat(comboBox2.SelectedItem.ToString()).Item2;
+                    if (category == "II")
+                    {
+                        currentPrice *= 2;
+                    }
+                    if (category == "III")
+                    {
+                        currentPrice *= 3;
+                    }
+                    if (category == "Высшая") { currentPrice *= 4; }
+                }
+            }
+            textBox1.Text = currentPrice.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == null || comboBox2.SelectedItem == null)
+            {
+                MessageBox.Show("Не заполнено одно или несколько полей: выбор мастера и/или выбор услуги");
+            }
+            else
+            {
+                var selectedService = comboBox1.SelectedItem.ToString();
+                var selectedMaster = comboBox2.SelectedItem.ToString();
+
+                var masterArgs = getIdAndCat(selectedMaster);
+
+                int selectedMasterId = masterArgs.Item1;
+                string selectedMasterCategory = masterArgs.Item2;
+
+                int servicePrice = getPrice(selectedService);
+
+                string selectedDate = dateTimePicker1.Value.Date.ToString();
+
+                OleDbCommand cmd = new OleDbCommand($"INSERT INTO Bookings (client_id, master_id, service_title, insertion_date, appointment_date_time, price, masterСat) VALUES ({currentId}, {selectedMasterId}, '{selectedService}', '{selectedDate}', '{selectedDate}', {servicePrice}, '{selectedMasterCategory}')",mC);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var selectedService = comboBox1.SelectedItem.ToString();
+
+            OleDbCommand cmd = new OleDbCommand($"SELECT * FROM Masters LEFT JOIN Services ON Masters.specialization = Services.category WHERE Services.title = '{selectedService}'", mC);
+
+            var l = cmd.ExecuteReader();
+            comboBox2.Items.Clear();
+            while (l.Read()) comboBox2.Items.Add(l[1].ToString());
+            l.Close();
+
+            RefreshPrice();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshPrice();
+        }
+    }
+}
